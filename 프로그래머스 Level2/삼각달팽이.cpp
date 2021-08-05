f533278747f3f0f7cdce28ca74d6f988a389f829
JuYeong98 <77095811+JuYeong98@users.noreply.github.com>
2021-08-05 12:32:39 +0900

    Add files via upload

---
+++ 프로그래머스 Level2/삼각달팽이.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n) {
+    vector<int> answer;
+    //맨 위에랑 맨 아래 분리
+    /*
+      1          1     1      
+     26        23
+     345
+    */
+    vector<vector<int>> triangle(n); //n개의 행 생성
+    for (int i = 0; i < n; i++) {
+        
+    }
+
+    return answer;
+}
