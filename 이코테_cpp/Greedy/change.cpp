175bf6f152c0ba674cf4c86170878b0576cc0b7d
JuYeong98 <twinjuy@gmail.com>
2022-04-30 23:27:16 +0900

    "third"

--- 이코테_cpp/Greedy/change.cpp
+++ 이코테_cpp/Greedy/change.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 int main(void)
 {
     int money = 1260; 
     vector<int> moneys;
-    moneys.append(500);
-    moneys.append(100);
-    moneys.append(50);
-    moneys.append(10);
+    moneys.push_back(500);
+    moneys.push_back(100);
+    moneys.push_back(50);
+    moneys.push_back(10);
 
-    int num_of_changes =0
+    int num_of_changes =0;
     for(int i=0;i<moneys.size();i++){
         int changes_num = money/moneys[i];
         num_of_changes+=changes_num;
