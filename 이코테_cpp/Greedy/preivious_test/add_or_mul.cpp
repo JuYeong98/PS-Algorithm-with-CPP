f8b2c8d7c8663128a3ac669e8ebec9fd3d3f67a0
JuYeong98 <twinjuy@gmail.com>
2022-05-02 22:34:53 +0900

    previous test of greedy first

---
+++ 이코테_cpp/Greedy/preivious_test/add_or_mul.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    string N;
+    cin>>N;
+    int result =N[0]-'0';
+    for(int i=0;i<N.size()-1;i++){
+        if((result) * (N[i+1]-'0') >(result) + (N[i+1]-'0') ){
+            result *=(N[i+1]-'0');
+        }
+        else{
+            result +=(N[i+1]-'0');
+        }
+    }
+    cout<<result;
+}
